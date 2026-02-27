a37d17e6322b14654b493a15b908c020887d3482
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-02-27 13:36:35 +0545

    fixed protected route

--- src/protected/ProtectedRoute.jsx
+++ src/protected/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 import { Navigate } from "react-router-dom";
-import { isLoggedIn } from "./Auth"; // from the helper I suggested
-
-const ProtectedRoute = ({ element }) => {
-  if (!isLoggedIn()) {
+import { getUserRole } from "./Auth";
+ 
+const ProtectedRoute = ({ element, allowedRoles }) => {
+  const role = getUserRole();
+ 
+  if (!role || !allowedRoles.includes(role)) {
+    localStorage.removeItem("token");
     return <Navigate to="/login" replace />;
   }
-
+ 
   return element;
 };
-
+ 
 export default ProtectedRoute;
